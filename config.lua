dca20338639ea85598e64dde8e86335d9ea7f98b
GenixScripts <frstudios24@gmail.com>
2025-02-13 16:51:03 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,83 @@
+Config = {}
+
+
+Config.NPCs = {
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(372.47, 327.15, 102.60, 255.67),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(-1486.31, -377.38, 39.16, 136.28),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(1391.52, 3606.0, 33.98, 200.22),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(2677.6, 3279.44, 54.24, 325.97),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(1959.52, 3740.41, 31.34, 294.81),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(24.14, -1346.64, 28.5, 267.52),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(549.35, 2671.17, 41.16, 95.43),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(-3242.99, 999.9, 11.83, 349.35),  
+    },
+    {
+        model = "a_m_m_business_01",  
+        coords = vector4(1728.06, 6416.14, 34.04, 241.22),  
+    },
+
+    
+}
+
+Config.Items = {
+    {
+        name = "water",
+        label = "water",
+        price = 50,
+        stock = 100  
+    },
+    {
+        name = "phone",
+        label = "phone",
+        price = 50,
+        stock = 100  
+    },
+    {
+        name = "bandage",
+        label = "bandage",
+        price = 50,
+        stock = 100  
+    },
+    {
+        name = "radio",
+        label = "radio",
+        price = 50,
+        stock = 100  
+    },
+    {
+        name = "sandwich",
+        label = "sandwich",
+        price = 50,
+        stock = 100  
+    },
+    {
+        name = "repairkit",
+        label = "repairkit",
+        price = 50,
+        stock = 100  
+    },
+
+}
